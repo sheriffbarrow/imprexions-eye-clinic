37900d7ee4aad37a804fa62e94eb2a1a2337523d
sheriffbarrow <smsbarrowjnr@gmail.com>
2020-08-20 16:35:27 +0000

    added allowed host

--- eye_clinic/settings.py
+++ eye_clinic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9+0*jyb3rnmdc38gv0txjzx0_2#3)#kbekq-m18b3v-z41csfz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['imprexions-eye-clinic.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
